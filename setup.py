a7acfc5062f236f321a664c976b2bc0552c07de7
JonGerrand <gerrand.jonathan@gmail.com>
2020-02-05 13:41:30 +0200

    Add new repo paths

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         description='EDSA example python package 2020',
         long_description=open('README.md').read(),
         install_requires=['numpy'],
-        url='https://github.com/<username>/<package-name>',
+        url='https://github.com/JonGerrand/mypackage.git',
         author='Zeus',
         author_email='zeus@explore-ai.net'
 )
